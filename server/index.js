e83e5aa4d09b68e033ca4524a721f3ad8452d4e2
DigitalHerencia <digitalherencia@outlook.com>
2024-09-05 18:06:19 -0600

    server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import express from "express"
 import helmet from "helmet"
 import mongoose from "mongoose"
 import morgan from "morgan"
+import path from "path"
 import breakdownRoute from "./routes/breakdownRoute.js"
 import cityRoute from "./routes/cityRoute.js"
 import comparisonRoutes from "./routes/comparisonRoute.js"
@@ -14,26 +15,21 @@ import menusRoute from "./routes/menusRoute.js"
 import salesRoute from "./routes/salesRoute.js"
 import usersRoute from "./routes/usersRoute.js"
 
+// Load environment variables
 dotenv.config()
+
+// Initialize express app
 const app = express()
+
+// Middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 app.use(morgan("common"))
-app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
-// Registering Routes
-app.use("/users", usersRoute)
-app.use("/menus", menusRoute)
-app.use("/sales", salesRoute)
-app.use("/dispensaries", dispensariesRoute)
-app.use("/city", cityRoute)
-app.use("/kpi", kpiRoute)
-app.use("/comparison", comparisonRoutes)
-app.use("/breakdown", breakdownRoute)
-
-// MongoDB Connection
+// MongoDB connection
 mongoose
     .set("strictQuery", false)
     .connect(process.env.MONGO_URI, {
@@ -45,11 +41,35 @@ mongoose
         console.error("Failed to connect to MongoDB Atlas", error)
     )
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// Routes
+app.use("/users", usersRoute)
+app.use("/menus", menusRoute)
+app.use("/sales", salesRoute)
+app.use("/dispensaries", dispensariesRoute)
+app.use("/city", cityRoute)
+app.use("/kpi", kpiRoute)
+app.use("/comparison", comparisonRoutes)
+app.use("/breakdown", breakdownRoute)
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname, "../client/build")))
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../client", "build", "index.html")
+        )
+    })
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send("Something broke!")
+})
+
+// Set the PORT and start the server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
