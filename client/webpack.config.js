f7b169cfab1c3796e551666e95add0ffad84e77f
DigitalHerencia <digitalherencia@outlook.com>
2024-09-07 14:09:52 -0600

    packed out

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,42 @@
-import { resolve } from "path"
-
-export const entry = "./src/index.js"
+import { resolve as _resolve, join } from "path"
+export const mode = "development"
+export const entry = join(__dirname, "app", "index")
+export const watch = true
 export const output = {
-    filename: "main.js",
-    path: resolve(__dirname, "dist"),
+    path: join(__dirname, "dist"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+    chunkFilename: "[name].js",
+}
+export const module = {
+    rules: [
+        {
+            test: /.jsx?$/,
+            include: [_resolve(__dirname, "app")],
+            exclude: [_resolve(__dirname, "node_modules")],
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    [
+                        "@babel/env",
+                        {
+                            targets: {
+                                browsers: "last 2 chrome versions",
+                            },
+                        },
+                    ],
+                ],
+            },
+        },
+    ],
+}
+export const resolve = {
+    extensions: [".json", ".js", ".jsx"],
+}
+export const devtool = "source-map"
+export const devServer = {
+    contentBase: join(__dirname, "/dist/"),
+    inline: true,
+    host: "localhost",
+    port: 8080,
 }
